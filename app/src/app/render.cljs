41c90ffe54491b616d02da54a1d8f2d9813c6a2b
jiyinyiyong <jiyinyiyong@gmail.com>
2017-10-02 15:41:09 +0800

    config CDN

--- app/src/app/render.cljs
+++ app/src/app/render.cljs
@@ -12,20 +12,28 @@
    ""
    (merge
     base-info
-    {:styles [], :scripts ["/main.js" "/browser/lib.js" "/browser/main.js"]})))
+    {:styles ["http://localhost:8100/main.css"],
+     :scripts ["/main.js" "/browser/lib.js" "/browser/main.js"]})))
+
+(def preview? (= "preview" js/process.env.prod))
 
 (defn prod-page []
   (let [html-content (make-string (comp-container {} nil))
         manifest (.parse js/JSON (slurp "dist/assets-manifest.json"))
-        cljs-manifest (.parse js/JSON (slurp "dist/manifest.json"))]
+        cljs-manifest (.parse js/JSON (slurp "dist/manifest.json"))
+        cdn (if preview? "" "http://repo-cdn.b0.upaiyun.com/cumulo-workflow/")
+        prefix-cdn (fn [x] (str cdn x))]
     (make-page
      html-content
      (merge
       base-info
-      {:styles [(aget manifest "main.css")],
-       :scripts [(aget manifest "main.js")
-                 (-> cljs-manifest (aget 0) (aget "js-name"))
-                 (-> cljs-manifest (aget 1) (aget "js-name"))]}))))
+      {:styles ["http://repo-cdn.b0.upaiyun.com/favored-fonts/main.css"
+                (prefix-cdn (aget manifest "main.css"))],
+       :scripts (map
+                 prefix-cdn
+                 [(aget manifest "main.js")
+                  (-> cljs-manifest (aget 0) (aget "js-name"))
+                  (-> cljs-manifest (aget 1) (aget "js-name"))])}))))
 
 (defn main! []
   (if (= js/process.env.env "dev")
