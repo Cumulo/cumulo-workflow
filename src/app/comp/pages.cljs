e6bba733c0ee9ff1fab5c3004b2c1548fe3792e6
jiyinyiyong <jiyinyiyong@gmail.com>
2018-06-05 01:50:15 +0800

    remove js/prompt with comp-prompt

--- src/app/comp/pages.cljs
+++ src/app/comp/pages.cljs
@@ -4,16 +4,20 @@
             [app.schema :as schema]
             [respo-ui.core :as ui]
             [respo-ui.colors :as colors]
-            [respo.macros :refer [defcomp list-> button <> span div a]]
+            [respo.macros :refer [defcomp list-> cursor-> button <> span div a]]
             [respo.comp.space :refer [=<]]
             [clojure.string :as string]
             [respo-ui.comp.icon :refer [comp-icon]]
-            [respo-alerts.comp.alerts :refer [comp-confirm]]))
+            [respo-alerts.comp.alerts :refer [comp-confirm comp-prompt]]
+            [respo.comp.inspect :refer [comp-inspect]]))
 
 (defcomp
  comp-pages
  (states router-data)
- (let [state (or (:data states) {:delete-title {:show? false, :data nil}})]
+ (let [state (or (:data states)
+                 {:delete-title {:show? false, :data nil},
+                  :update-title {:show? false, :data nil},
+                  :create-title {:show? false, :data nil}})]
    (div
     {:style {:padding 16}}
     (list->
@@ -37,9 +41,7 @@
                 (span
                  {:style {:cursor :pointer},
                   :on-click (fn [e d! m!]
-                    (let [new-title (js/prompt "A new title?" (:title page))]
-                      (when (not (string/blank? new-title))
-                        (d! :page/update-title {:id (:id page), :title new-title}))))}
+                    (m! (assoc state :update-title {:show? true, :data page})))}
                  (comp-icon :compose))
                 (=< 8 nil)
                 (span
@@ -49,9 +51,7 @@
                  (comp-icon :ios-trash))))]))))
     (button
      {:style ui/button,
-      :on-click (fn [e d! m!]
-        (let [title (js/prompt "Page title?")]
-          (when (not (string/blank? title)) (d! :page/create title))))}
+      :on-click (fn [e d! m!] (m! (assoc state :create-title {:show? true, :data nil})))}
      (<> "Add"))
     (let [info (:delete-title state)]
       (when (:show? info)
@@ -59,4 +59,27 @@
          "Are you sure to delete?"
          (fn [result d! m!]
            (when result (d! :page/remove-one (:data info)))
-           (m! (assoc state :delete-title {:show? false, :data nil})))))))))
+           (m! (assoc state :delete-title {:show? false, :data nil}))))))
+    (let [info (:update-title state), page (:data info)]
+      (when (:show? info)
+        (cursor->
+         :prompt
+         comp-prompt
+         states
+         "Add a new title:"
+         (:title page)
+         (fn [result d! m!]
+           (when (not (string/blank? result))
+             (d! :page/update-title {:id (:id page), :title result}))
+           (m! %cursor (assoc state :update-title {:show? false, :data nil}))))))
+    (let [info (:create-title state)]
+      (when (:show? info)
+        (cursor->
+         :prompt
+         comp-prompt
+         states
+         "A title:"
+         ""
+         (fn [result d! m!]
+           (when (not (string/blank? result)) (d! :page/create result))
+           (m! %cursor (assoc state :create-title {:show? false, :data nil})))))))))
