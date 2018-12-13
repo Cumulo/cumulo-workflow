f616b0ef00159c875b301a131df6f4fba92262cf
jiyinyiyong <jiyinyiyong@gmail.com>
2018-12-14 02:29:51 +0800

    merge service.cljs into server.cljs

--- src/app/server.cljs
+++ src/app/server.cljs
@@ -1,7 +1,6 @@
 
 (ns app.server
   (:require [app.schema :as schema]
-            [app.service :refer [run-server! sync-clients!]]
             [app.updater :refer [updater]]
             [cljs.reader :refer [read-string]]
             [cumulo-reel.reel :refer [reel-reducer refresh-reel reel-schema]]
@@ -11,7 +10,14 @@
             [app.node-config :as node-config]
             [app.node-config :refer [dev?]]
             [app.config :as config]
-            [cumulo-util.file :refer (write-mildly!)]))
+            [cumulo-util.file :refer (write-mildly!)]
+            [cljs.nodejs :as nodejs]
+            [app.twig.container :refer [twig-container]]
+            [recollect.diff :refer [diff-twig]]
+            [recollect.twig :refer [render-twig]]
+            [ws-edn.server :refer [wss-serve! wss-send! wss-each!]]))
+
+(defonce *client-caches (atom {}))
 
 (def initial-db
   (let [filepath (:storage-path node-config/env)]
@@ -53,10 +59,40 @@
   (comment println "exit code is:" (pr-str code))
   (.exit js/process))
 
+(defn sync-clients! [reel]
+  (wss-each!
+   (fn [sid socket]
+     (let [db (:db reel)
+           records (:records reel)
+           session (get-in db [:sessions sid])
+           old-store (or (get @*client-caches sid) nil)
+           new-store (render-twig (twig-container db session records) old-store)
+           changes (diff-twig old-store new-store {:key :id})]
+       (println "Changes for" sid ":" changes (count records))
+       (if (not= changes [])
+         (do
+          (wss-send! sid {:kind :patch, :data changes})
+          (swap! *client-caches assoc sid new-store)))))))
+
 (defn render-loop! []
   (if (not (identical? @*reader-reel @*reel))
     (do (reset! *reader-reel @*reel) (sync-clients! @*reader-reel)))
   (js/setTimeout render-loop! 200))
+
+(defn run-server! [on-action! port]
+  (wss-serve!
+   port
+   {:on-open! (fn [sid socket]
+      (on-action! :session/connect nil sid)
+      (.info js/console "New client.")),
+    :on-data! (fn [sid action]
+      (case (:kind action)
+        :op (on-action! (:op action) (:data action) sid)
+        (println "unknown data" action))),
+    :on-close! (fn [sid event]
+      (.warn js/console "Client closed!")
+      (on-action! :session/disconnect nil sid)),
+    :on-error! (fn [error] (.error js/console error))}))
 
 (defn main! []
   (run-server! #(dispatch! %1 %2 %3) (:port config/site))
