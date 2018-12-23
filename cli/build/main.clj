1cad1129de0c1d8f9e9ffa347cca3295b0d22417
jiyinyiyong <jiyinyiyong@gmail.com>
2018-12-24 01:47:49 +0800

    moving upload script to shadow-cljs

--- cli/build/main.clj
+++ cli/build/main.clj
@@ -12,6 +12,7 @@
   (shadow/release :client)
   (shadow/release :server)
   (shadow/compile :page)
+  (shadow/compile :upload)
   (sh! "release=true cdn=true node target/page.js")
   (sh! "cp package.json dist/")
   (sh! "cp entry/manifest.json dist/"))
